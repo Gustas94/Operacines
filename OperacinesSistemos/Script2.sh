f5514ee80f48de49936799fa8182bd7bb4da8464
BuildTools <unconfigured@null.spigotmc.org>
2024-05-22 17:20:49 +0300

    Fixes.

--- OperacinesSistemos/Script2.sh
+++ OperacinesSistemos/Script2.sh
@@ -6,7 +6,7 @@
 target=$(( RANDOM % 100 + 1 ))
 
 # Inicializuojami kintamieji
-spėjimas=0
+spejimas=0
 bandymai=0
 
 echo "Sveiki atvykę į Skaičių Spėjimo Žaidimą!"
@@ -15,15 +15,15 @@ echo "Aš pasirinkau skaičių tarp 1 ir 100. Ar galite jį atspėti?"
 # Ciklas, kol vartotojas atspės teisingą skaičių
 while [ $spėjimas -ne $target ]; do
     # Nuskaitomas vartotojo įvestis
-    read -p "Įveskite savo spėjimą: " spėjimas
+    read -p "Įveskite savo spėjimą: " spejimas
 
     # Padidinamas bandymų skaitiklis
     bandymai=$(( bandymai + 1 ))
 
     # Patikrinama, ar spėjimas teisingas
-    if [ $spėjimas -lt $target ]; then
+    if [ $spejimas -lt $target ]; then
         echo "Per mažas! Bandykite dar kartą."
-    elif [ $spėjimas -gt $target ]; then
+    elif [ $spejimas -gt $target ]; then
         echo "Per didelis! Bandykite dar kartą."
     else
         echo "Sveikiname! Jūs atspėjote teisingą skaičių per $bandymai bandymus."
